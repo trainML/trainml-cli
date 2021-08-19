bdd2984b7f9cf95a34e87e20f2d9ba9fdf36b267
Adam Kowalski <adam@trainml.ai>
2021-08-18 21:58:52 -0500

    fixing notebook url

--- trainml/jobs.py
+++ trainml/jobs.py
@@ -175,7 +175,7 @@ class Job:
     def notebook_url(self) -> str:
         if self.type != "notebook":
             return None
-        return f"{self.url}/{self.id}/?token={self._job.get('nb_token')}"
+        return f"{self.url}/?token={self._job.get('nb_token')}"
 
     def __str__(self):
         return json.dumps(self.dict)
